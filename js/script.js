8955f5773886d2399edbc2c8f031c8f87706fd42
luztherose <luzdelarosaramirez@gmail.com>
2020-07-25 10:31:06 -0400

    updating index.html

--- js/script.js
+++ js/script.js
@@ -23,60 +23,51 @@ app.questionsArray = [
         answer:'Ada Lovelace'
     }
 ];
-//console.log(app.questionsArray[0].q1.removeClass('allQuestions'));
-
 // generateRandomNumber
 function generateRandomNumber() {
     return Math.floor(Math.random() * app.questionsArray.length);
 }
 // store random number
 app.readRandomNumber = generateRandomNumber()
-
 //Looping thr the array
-let questionDisplayed;
+app.questionDisplayed;
 app.displayRandomQuestion = function () {
     for (let i = 0; i < app.questionsArray.length; i++) {
         if (i === app.readRandomNumber) {
-            questionDisplayed = app.questionsArray[i].question
-            questionDisplayed.removeClass('allQuestions')
+            app.questionDisplayed = app.questionsArray[i].question
+            app.questionDisplayed.removeClass('allQuestions')
         }
     }
-    console.log(app.readRandomNumber);
-    console.log(questionDisplayed);
 }
 // Find the correct answer 
 app.getCorrectAnswer = function() {
     for(let i = 0; i < app.questionsArray.length; i++) {
         let current = app.questionsArray[i];
-        if (current.question === questionDisplayed) {
-            console.log(current.question);
-            console.log(questionDisplayed);
+        if (current.question === app.questionDisplayed) {
             return current.answer;
         } 
     }
 }
-
 // h1Heading
 app.h1Heading = $('h1');
 // intro button
 app.playButton = $('.introScreenButton').on('click', function () {
+    $('.gameRules').addClass('button');
     app.displayRandomQuestion();
     $('.submitForm').removeClass('button');
     app.playButton.addClass('button');
     app.h1Heading.text('Select One Answer!!');
 });
-
 //Add event Listener
 $('form').on('submit', function (e) {
     e.preventDefault();
     //Get user's answer to the question
-    
-    const userAnwer = questionDisplayed.find('input[type=radio]:checked').val(); // UserAnwer
+    const userAnwer = app.questionDisplayed.find('input[type=radio]:checked').val(); // UserAnwer
     // Get correct answer
     const correctAnswer = app.getCorrectAnswer();
     // Validating user's choice 
     if(userAnwer === correctAnswer) {
-        console.log('you selected the right anwer');
+        alert('you selected the right anwer');
     }else {
         alert('wrong answer');
         location.reload(); 
