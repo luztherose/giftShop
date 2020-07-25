67641e85216f5939fc82906d49999e4081d5f490
luztherose <luzdelarosaramirez@gmail.com>
2020-07-25 19:57:22 -0400

    giftShop features:gift:

--- js/script.js
+++ js/script.js
@@ -8,21 +8,66 @@ app.questionsArray = [
     },
     {
         question: $('#question2'),
-        answer:  'Vatican City'
+        answer: 'Vatican City'
     },
     {
         question: $('#question3'),
-        answer:'Chandra Bahadur'
+        answer: 'Chandra Bahadur'
     },
     {
         question: $('#question4'),
-        answer:'Malala Yousafzai'
+        answer: 'Malala Yousafzai'
     },
     {
         question: $('#question5'),
-        answer:'Ada Lovelace'
+        answer: 'Ada Lovelace'
     }
 ];
+app.safeGift = [
+    {
+        artist: 'Farshad Rezvanian',
+        src: 'assets/farshadRezvanian.jpg'
+    },
+    {
+        artist: 'Tim Mossholder',
+        src: 'assets/timMossholder.jpg'
+    },
+    {
+        artist: 'Mick Haupt',
+        src: 'assets/MickHaupt.jpg'
+    },
+    {
+        artist: 'Sebastian Coman',
+        src: 'assets/sebastianComan.jpg'
+    },
+    {
+        artist: 'Wilhelm Gunkel',
+        src: 'assets/wilhelmGunkel.jpg'
+    }
+];
+app.supriseGift = [
+    {
+        source:'Queen/YouTube video',
+        url:'assets/weAreTheChampions.mp4'
+    },
+    {
+        source:'YouTube video',
+        url:'assets/funnyBabiesCompilation.mp4'
+    },
+    {
+        source:'YouTube video',
+        url:'assets/BabiesPerformed.mp4'
+    },
+    {
+        source:'YouTube video',
+        url:'assets/bulldogPuppy.mp4'
+    },
+    {
+        source:'YouTube video',
+        url:'assets/funnyAnimalCollectionVideos.mp4'
+    }
+];
+console.log(app.supriseGift);
 // generateRandomNumber
 function generateRandomNumber() {
     return Math.floor(Math.random() * app.questionsArray.length);
@@ -40,12 +85,12 @@ app.displayRandomQuestion = function () {
     }
 }
 // Find the correct answer 
-app.getCorrectAnswer = function() {
-    for(let i = 0; i < app.questionsArray.length; i++) {
+app.getCorrectAnswer = function () {
+    for (let i = 0; i < app.questionsArray.length; i++) {
         let current = app.questionsArray[i];
         if (current.question === app.questionDisplayed) {
             return current.answer;
-        } 
+        }
     }
 }
 // h1Heading
@@ -58,7 +103,10 @@ app.playButton = $('.introScreenButton').on('click', function () {
     app.playButton.addClass('button');
     app.h1Heading.text('Select One Answer!!');
 });
-//Add event Listener
+//Selecting elements with Vanilla JS
+app.buzzerRightAnwer = document.getElementById('buzzerRightAnswer');
+app.buzzerWrongAnwer = document.getElementById('buzzerWrongAnswer');
+//Add event Listener to the form
 $('form').on('submit', function (e) {
     e.preventDefault();
     //Get user's answer to the question
@@ -66,14 +114,52 @@ $('form').on('submit', function (e) {
     // Get correct answer
     const correctAnswer = app.getCorrectAnswer();
     // Validating user's choice 
-    if(userAnwer === correctAnswer) {
-        alert('you selected the right anwer');
-    }else {
-        alert('wrong answer');
-        location.reload(); 
+    if (userAnwer === correctAnswer) {
+        app.buzzerRightAnwer.play();
+        //add backgroungColor to the user answer
+        app.h1Heading.text('Pick Your Gift 🎁');
+        // hide current question
+        app.questionDisplayed.addClass('allQuestions');
+        // hide submit button
+        $('.submitForm').addClass('button');
+        // display a message
+        alert('Welcome to Our GiftShop')
+        // Select giftShop Buttons
+        const supriseGift = $('.supriseGift');
+        const safeGift = $('.safeGift');
+        // show giftShop buttons
+        supriseGift.removeClass('button');
+        safeGift.removeClass('button');
+        // add an Event listener to the giftShop Buttons
+        supriseGift.on('click', () => {
+            // diplay a random video
+            for (let i = 0; i < app.supriseGift.length; i++) {
+                if (i === app.readRandomNumber) {
+                    const videoUrl = app.supriseGift[i].url;
+                    const videoSource = $('<p>').text(`Videos from ${app.supriseGift[i].source}`);
+                    const videoTag = $('<video controls>').attr('src', videoUrl);
+                    const videoContainer = $('<div>').append(videoTag, videoSource);
+                    $('.videoContainer').append(videoContainer);
+                }
+            }
+        });
+        safeGift.on('click', () => {
+            // look through the object
+            // diplay one image randomly
+            for (let i = 0; i < app.safeGift.length; i++) {
+                if (i === app.readRandomNumber) {
+                    const imgSource = app.safeGift[i].src;
+                    const artistName = $('<p>').text(`Photo by ${app.safeGift[i].artist} on Unsplash`);
+                    const img = $('<img>').attr('src', imgSource);
+                    img.attr('alt', 'outdoor image');
+                    const imgContainer = $('<div>').append(img, artistName);
+                    $('.selectedGift').append(imgContainer);
+                }
+            }
+        });
+    } else {
+        app.buzzerWrongAnwer.play();
+        //location.reload();
     }
-    // If the user's anwer in correct :
-    // then, I will print you selected the right anwer
-    // else, I print your anwer was incorrect
 });
 
